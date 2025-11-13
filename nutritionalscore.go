f2e099ac3286425d58fd56b8bb59d4a167b6300d
Fahril Hadi <fahrilhadi77@gmail.com>
2025-11-13 21:23:03 +0700

    energy and sodium methods

--- nutritionalscore.go
+++ nutritionalscore.go
@@ -69,6 +69,14 @@ func (p ProteinGram) GetPoints(st ScoreType) int {
 	
 }
 
+func EnergyFromKcal(kcal float64) EnergyKJ {
+	return EnergyKJ(kcal * 4.184)
+}
+
+func SodiumFromSalt(SaltMg float64) SodiumMilligram {
+	return SodiumMilligram(SaltMg / 2.5)
+}
+
 func GetNutritionalScore(n NutritionalData, st ScoreType) NutritionalScore {
 	value := 0
 	positive := 0
